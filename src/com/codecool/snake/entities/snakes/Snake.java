d97ebbaf1179a4d5f972564d1641a0a4bd9630c6
Anna <anna.iklody@gmail.com>
2018-12-11 16:09:25 +0100

    post-game alert added, game exitable

--- src/com/codecool/snake/entities/snakes/Snake.java
+++ src/com/codecool/snake/entities/snakes/Snake.java
@@ -1,13 +1,22 @@
 package com.codecool.snake.entities.snakes;
 
 import com.codecool.snake.DelayedModificationList;
+import com.codecool.snake.Display;
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.eventhandler.InputHandler;
 
 import com.sun.javafx.geom.Vec2d;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.input.KeyCode;
+import javafx.application.Platform;
+
+import javax.xml.transform.Result;
+import java.util.Optional;
+
+import static com.codecool.snake.Display.*;
 
 
 public class Snake implements Animatable {
@@ -61,7 +70,36 @@ public class Snake implements Animatable {
         if (head.isOutOfBounds() || health <= 0) {
             System.out.println("Game Over");
             Globals.getInstance().stopGame();
+            displayAlert();
         }
+    }
+
+    public void displayAlert() {
+
+     Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("GAME OVER!");
+        alert.setHeaderText("Your score is: ");
+        alert.setContentText("Start a new game?");
+        ButtonType buttonTypeOne = new ButtonType("Yes");
+        ButtonType buttonTypeTwo = new ButtonType("No");
+
+        Platform.runLater(() -> {
+            Optional<ButtonType> result = alert.showAndWait();
+/*            if (result.isPresent() && result.get() == buttonTypeOne) {
+                System.out.println("testing Yes");
+            } else if (result.isPresent() && result.get() == buttonTypeTwo) {
+                System.out.println("testing no");
+                Platform.exit();
+            }*/
+            if(result.get() == ButtonType.OK) {
+                System.out.println("testing ok");
+            }
+            else if(result.get() == ButtonType.CANCEL) {
+                System.out.println("testing cancel");
+                Platform.exit();
+            }
+        });
+
     }
 
     private void updateSnakeBodyHistory() {
