6beacf04e0063ea517a522d8b0563d8d089876ee
Krisztina Nagy <nakuaat@gmail.com>
2018-12-11 22:27:53 +0100

    change enemy dmg to negativ

--- src/com/codecool/snake/entities/enemies/SimpleEnemy.java
+++ src/com/codecool/snake/entities/enemies/SimpleEnemy.java
@@ -20,7 +20,7 @@ public class SimpleEnemy extends Enemy implements Animatable, Interactable {
     int speed = 1;
 
     public SimpleEnemy() {
-        super(10);
+        super(-10);
 
         setImage(Globals.getInstance().getImage("SimpleEnemy"));
         setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
