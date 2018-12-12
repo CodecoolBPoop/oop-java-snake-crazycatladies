42d15c810659b64bd6a67fcaa93a3c0be0c2965c
Krisztina Nagy <nakuaat@gmail.com>
2018-12-12 19:28:01 +0100

    the class implement two interfaces

--- src/com/codecool/snake/entities/enemies/Enemy.java
+++ src/com/codecool/snake/entities/enemies/Enemy.java
@@ -1,8 +1,10 @@
 package com.codecool.snake.entities.enemies;
 
+import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.entities.Interactable;
 
-public abstract class Enemy extends GameEntity{
+public abstract class Enemy extends GameEntity implements Animatable, Interactable {
     private final int damage;
 
     public Enemy(int damage) {
